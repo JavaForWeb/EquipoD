11fc7f9b485c96310deb2c9798642858809127f7
leoyml <leoyml@gmail.com>
2016-10-08 10:12:32 -0400

    primer comentario

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+	This is a simple Java Program.
+	Call this file HelloWorld.java
+*/
+class HelloWorld{
+//A Java program begins with a call to main().
+public static void main(String args[]){
+	System.out.println("Hello from Java!!");
+	}
+}
